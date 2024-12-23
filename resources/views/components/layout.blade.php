4e499a3fef896314b8e0c29d6d794e1f49ee9afe
Clintelson <elsonsorianoclint@gmail.com>
2024-12-23 12:46:25 +0800

    feat: medicine components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,51 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     @vite('resources/css/app.css')
     <title>CMS</title>
 
 </head>
+
 <body>
+    <div class="flex flex-row w-screen h-dvh">
+        <nav class="w-fit h-dvh flex flex-col bg-[#FDF6EC] px-10 py-12 border-r rounded-r-3xl justify-between">
+            <div class="flex flex-col items-center justify-center w-full gap-8 px-5 mt-16 font-semibold h-fit ">
+                <x-nav-link href="/dashboard" :active="request()->is('dashboard')">
+                    Dashboard
+                </x-nav-link>
+                <x-nav-link href="/medicine" :active="request()->is('medicine') || request()->is('*product')">
+                    Medicine
+                </x-nav-link>
+                <x-nav-link href="/equipments" :active="request()->is('equipments')">
+                    Equipments
+                </x-nav-link>
+                <x-nav-link href="/accounts" :active="request()->is('accounts')">
+                    Accounts
+                </x-nav-link>
+            </div>
+            <form method="POST" action="/logout">
+                @csrf
+                @method("POST")
+                {{-- <x-button-logout>Log out</x-button-logout> --}}
+            </form>
+        </nav>
 
-    <nav class="w-fit h-dvh flex flex-col bg-[#FDF6EC] px-10 py-12 border-r rounded-r-3xl justify-between">
+        <div class="w-full h-dvh flex flex-col">
+            <div class="w-full h-fit bg-transparent">
+                <div class="flex flex-row items-center justify-between px-20 py-6">
+                    <div>Navigation Url</div>
+                    <div>Profile</div>
+                </div>
+            </div>
 
-        <div class="w-full h-fit flex flex-col gap-8 mt-16 px-5 justify-center   items-center font-semibold ">
-
-            <x-nav-link href="/dashboard" :active="request()->is('dashboard')">
-                Dashboard
-            </x-nav-link>
-
-            <x-nav-link href="/medicine" :active="request()->is('medicine') || request()->is('*product')">
-                Medicine
-            </x-nav-link>
-
-
-
-            <x-nav-link href="/equipments" :active="request()->is('equipments')">
-                Equipments
-            </x-nav-link>
-            <x-nav-link href="/accounts" :active="request()->is('accounts')">
-                Accounts
-            </x-nav-link>
+            {{ $slot }}
 
         </div>
-
-        <form method="POST" action="/logout">
-            @csrf
-            @method("POST")
-            {{-- <x-button-logout>Log out</x-button-logout> --}}
-       </form>
-    </nav>
-
-
-
-        {{ $slot }}
-
-
+    </div>
 
 </body>
+<script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+
 </html>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,13 @@
-<x-layout>hello</x-layout>
+<x-layout>
+    
+<div class="grid content-center w-full grid-cols-3 gap-4 h-dvh justify-items-center">
+
+<div class="col-span-2 border">Graph.1</div>
+<div class="border">Graph.2</div>
+<div class="border">Lorem</div>
+<div class="border">Lorem</div>
+<div class="border">Lorem</div>
+
+</div>
+
+</x-layout>
