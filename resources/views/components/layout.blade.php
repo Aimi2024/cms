ddded1f02ef34359dea5ed37a4f38848e6f0bb13
unknown <jeroldnoynay123@gmail.com>
2024-12-23 11:22:35 +0800

    fixed:sidnav

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,20 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
     @vite('resources/css/app.css')
     <title>CMS</title>
 
 </head>
 <body>
 
-<nav>
-    <x-nav-link href="/" :active="request()->is('index')">
-        Dashboard
-    </x-nav-link>
-    <x-nav-link href="/medicine" :active="request()->is('index')">
-        medicine
-    </x-nav-link>
-</nav>
+    <nav class="w-fit h-dvh flex flex-col bg-[#FDF6EC] px-10 py-12 border-r rounded-r-3xl justify-between">
+
+        <div class="w-full h-fit flex flex-col gap-8 mt-16 px-5 justify-center   items-center font-semibold ">
+
+            <x-nav-link href="/dashboard" :active="request()->is('dashboard')">
+                Dashboard
+            </x-nav-link>
+
+            <x-nav-link href="/medicine" :active="request()->is('medicine') || request()->is('*product')">
+                Medicine
+            </x-nav-link>
+
+
+
+            <x-nav-link href="/equipments" :active="request()->is('equipments')">
+                Equipments
+            </x-nav-link>
+            <x-nav-link href="/accounts" :active="request()->is('accounts')">
+                Accounts
+            </x-nav-link>
+
+        </div>
+
+        <form method="POST" action="/logout">
+            @csrf
+            @method("POST")
+            {{-- <x-button-logout>Log out</x-button-logout> --}}
+       </form>
+    </nav>
 
 
 
