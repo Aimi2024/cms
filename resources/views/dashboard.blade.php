97c4655b56cc656cce4b0b582e1c1ab7b3350b82
Clintelson <elsonsorianoclint@gmail.com>
2024-12-28 01:47:44 +0800

    feat: hover:dashboard items

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,19 +15,19 @@
         <div class="flex flex-row justify-between w-full">
 
             <div
-                class="border flex flex-col items-center bg-white text-red-500 font-extrabold justify-center w-32 h-32 overflow-hidden whitespace-nowrap">
+                class="border flex flex-col items-center bg-white text-red-500 font-extrabold justify-center w-32 h-32 overflow-hidden whitespace-nowrap hover:bg-gray-200">
                 <h1 class="text-4xl">2</h1>
                 <p class="text-sm">LOW STOCK</p>
             </div>
 
             <div
-                class="border flex flex-col items-center bg-white text-green-500 font-extrabold justify-center w-32 h-32 overflow-hidden whitespace-nowrap">
+                class="border flex flex-col items-center bg-white text-green-500 font-extrabold justify-center w-32 h-32 overflow-hidden whitespace-nowrap hover:bg-gray-200">
                 <h1 class="text-4xl">2</h1>
                 <p class="text-sm">NEW STOCKS</p>
             </div>
 
             <div
-                class="border flex flex-col items-center bg-white text-black font-extrabold justify-center w-32 h-32 overflow-hidden whitespace-nowrap">
+                class="border flex flex-col items-center bg-white text-black font-extrabold justify-center w-32 h-32 overflow-hidden whitespace-nowrap hover:bg-gray-200">
                 <h1 class="text-4xl">2</h1>
                 <p class="text-sm">EXPIRED</p>
             </div>
